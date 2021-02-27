dd851338d7d94eaa1cc984a2725ececea743b7b2
Bruno Ferreira <brunoferreirapedraca@gmail.com>
2021-02-27 17:49:31 -0300

    full

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,73 @@
+import { useState, useEffect, useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import { CountdownContext } from '../contexts/CountdownContext'
+
+import styles from '../styles/components/CountDown.module.css'
+
+export default function CountDown(){
+  const { 
+    minutes, 
+    seconds, 
+    hasFinished, 
+    isActive, 
+    startCountdown, 
+    resetCountdown 
+  } = useContext(CountdownContext)
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+  const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0').split('')
+
+  
+  return(
+    <div>
+      <div className={styles.countDownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondsLeft}</span>
+          <span>{secondsRight}</span>
+        </div>
+       </div>
+
+      { hasFinished ? (
+        <button
+          disabled 
+          className={styles.countdownButton}
+        >
+          Ciclo encerrado 
+          <img src="icons/check_circle.svg" alt="Level"/>
+        </button>
+          
+        )
+        :
+        (
+          <>
+            { isActive ? 
+            (
+              <button 
+                type="button"
+                className={` ${styles.countdownButton} ${styles.countdownButtonActive}`}
+                onClick={resetCountdown}
+              >
+                Abandonar ciclo
+              </button>
+            ) 
+            : 
+            (
+              <button 
+                type="button"
+                className={styles.countdownButton}
+                onClick={startCountdown}
+              >
+                Iniciar um ciclo
+              </button>
+            )}
+          </>
+        )
+      }
+    </div>
+  )
+}
